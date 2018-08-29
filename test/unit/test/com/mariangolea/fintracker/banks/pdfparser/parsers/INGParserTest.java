e3881d057ff32a784651e98eb8dd00d456f56f5a
Marian Golea <git@github.com:mariangolea/pdftransactionparser.git>
2018-08-29 13:06:35 +0300

    Fixed connection to sonar cloud, unit tests recognized.

--- test/unit/test/com/mariangolea/fintracker/banks/pdfparser/parsers/INGParserTest.java
+++ test/unit/test/com/mariangolea/fintracker/banks/pdfparser/parsers/INGParserTest.java
@@ -17,76 +17,53 @@ import com.mariangolea.fintracker.banks.pdfparser.parsers.INGParser;
  *
  * @author mariangolea@gmail.com
  */
-public class INGParserTest {
+public class INGParserTest extends INGParser{
 
-	INGParserLocal parser = new INGParserLocal();
+    @Test
+    public void testCompleteDateParser() {
+        String input;
+        Date output;
+        Calendar calendar = Calendar.getInstance(INGParser.ROMANIAN_LOCALE);
+        Map<String, Integer> monthNames = calendar.getDisplayNames(Calendar.MONTH, Calendar.LONG,
+                INGParser.ROMANIAN_LOCALE);
+        for (Entry<String, Integer> entry : monthNames.entrySet()) {
+            // try to verify day, month and year based on month values.
+            int year = 2000 + entry.getValue();
+            int day = 1 + entry.getValue(); // Month indices start with 0, sa when constructing days add 1.
+            int month = entry.getValue();
+            input = day + " " + entry.getKey() + " " + year;
+            output = parseCompletedDate(input);
+            calendar.setTime(output);
+            assertEquals("Month value not as expected.", month, calendar.get(Calendar.MONTH));
+            assertEquals("Day value not as expected.", day, calendar.get(Calendar.DAY_OF_MONTH));
+            assertEquals("Year value not as expected.", year, calendar.get(Calendar.YEAR));
+        }
 
-	@Test
-	public void testCompletedateParser() {
-		String input;
-		Date output;
-		Calendar calendar = Calendar.getInstance(INGParser.ROMANIAN_LOCALE);
-		Map<String, Integer> monthNames = calendar.getDisplayNames(Calendar.MONTH, Calendar.LONG,
-				INGParser.ROMANIAN_LOCALE);
-		for (Entry<String, Integer> entry : monthNames.entrySet()) {
-			// try to verify day, month and year based on month values.
-			int year = 2000 + entry.getValue();
-			int day = 1 + entry.getValue(); // Month indices start with 0, sa when constructing days add 1.
-			int month = entry.getValue();
-			input = day + " " + entry.getKey() + " " + year;
-			output = parser.parseCompletedDate(input);
-			calendar.setTime(output);
-			assertEquals("Month value not as expected.", month, calendar.get(Calendar.MONTH));
-			assertEquals("Day value not as expected.", day, calendar.get(Calendar.DAY_OF_MONTH));
-			assertEquals("Year value not as expected.", year, calendar.get(Calendar.YEAR));
-		}
+        output = parseCompletedDate("gibberish");
+        assertTrue(output == null);
+    }
 
-		output = parser.parseCompletedDate("gibberish");
-		assertTrue(output == null);
-	}
+    @Test
+    public void testStartedDateParser() {
+        String input = "14-08-2018";
+        Date output = parseStartDate(input);
+        Calendar c = Calendar.getInstance();
+        c.setTime(output);
+        assertEquals("Day value not as expected.", 14, c.get(Calendar.DAY_OF_MONTH));
+        assertEquals("Month value not as expected.", 7, c.get(Calendar.MONTH)); // Months start from 0, so Agust is 7!
+        assertEquals("Year value not as expected.", 2018, c.get(Calendar.YEAR));
 
-	@Test
-	public void testStartedateParser() {
-		String input = "14-08-2018";
-		Date output = parser.parseStartDate(input);
-		Calendar c = Calendar.getInstance();
-		c.setTime(output);
-		assertEquals("Day value not as expected.", 14, c.get(Calendar.DAY_OF_MONTH));
-		assertEquals("Month value not as expected.", 7, c.get(Calendar.MONTH)); // Months start from 0, so Agust is 7!
-		assertEquals("Year value not as expected.", 2018, c.get(Calendar.YEAR));
+        output = parseStartDate("gibberish");
+        assertTrue(output == null);
+    }
 
-		output = parser.parseCompletedDate("gibberish");
-		assertTrue(output == null);
-	}
+    @Test
+    public void testAmount() {
+        String input = "1.195,60";
+        Float output = parseAmount(input);
+        assertTrue("Amount parsing failed.", (float) 1195.6 == output);
 
-	@Test
-	public void testAmount() {
-		String input = "1.195,60";
-		Float output = parser.parseAmount(input);
-		assertTrue("Amount parsing failed.", (float) 1195.6 == output);
-
-		output = parser.parseAmount("gibberish");
-		assertTrue(output == null);
-	}
-
-	/**
-	 * Allows calling individual protected methods directly.
-	 */
-	private class INGParserLocal extends INGParser {
-
-		@Override
-		protected Float parseAmount(String amountString) {
-			return super.parseAmount(amountString);
-		}
-
-		@Override
-		protected Date parseCompletedDate(String dateString) {
-			return super.parseCompletedDate(dateString);
-		}
-
-		@Override
-		protected Date parseStartDate(String dateString) {
-			return super.parseStartDate(dateString);
-		}
-	}
+        output = parseAmount("gibberish");
+        assertTrue(output == null);
+    }
 }
